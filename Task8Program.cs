6c3594d28d8e3b1e1d033f3c191b5ff083a7d3ef
Ekaterina Biyushkina <biyushkina.k@gmail.com>
2022-12-06 16:18:51 +0300

    Program done but functions incorrectly for negative numbers

---
+++ Task8Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число:() ");
+int n = int.Parse(Console.ReadLine());
+int a = 1;
+if(n>0)
+{
+    while(a<=n)
+    {
+        Console.WriteLine($"{a}");
+        a = a+1;
+    }
+}
+else
+{
+    while(a>=n)
+    Console.WriteLine($"{a}");
+    a = a-1;
+}
